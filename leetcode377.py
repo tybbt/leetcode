fb6399938093dc2f14a12f0fbeaf5ec440620e59
tybbt <875260315@qq.com>
2021-04-24 10:37:43 +0800

    dp

---
+++ leetcode377.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return 0
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        for i in range(1, target + 1):
+            for num in nums:
+                if i >= num:
+                    dp[i] += dp[i - num]
+        return dp[target]
+
+
+sv = Solution()
+nums = [1,2,3]
+target = 4
+print(sv.combinationSum4(nums, target))
